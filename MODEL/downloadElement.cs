bd8327c5e9b4724580190e5abdac3e53ddcf3223
Krzysztof Stanek <ekstanek@gmail.com>
2023-08-19 18:29:19 +0200

    downloadElement class

---
+++ MODEL/downloadElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YT2MP3.MODEL
+{
+    internal class downloadElement
+    {
+        public String url;
+        public String title;
+        public String description;
+        public String videoVersionName;
+        public String audioVersionName;
+        public Int32 status;
+    }
+}
